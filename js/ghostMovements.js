0e56f5564cbde7afa7dfb387db76f8fa921f4969
Nimira43 <n.rabone72@aol.com>
2025-08-11 18:55:48 +0100

    while loop - 2. randomMovement function done for now

--- js/ghostMovements.js
+++ js/ghostMovements.js
@@ -5,11 +5,13 @@ export function randomMovement(position, direction, objectExist) {
   let nextMovePos = position + dir.movement
   const keys = Object.keys(directions)
 
-
   while (
     objectExist(nextMovePos, objectType.wall) ||
     objectExist(nextMovePos, objectType.ghost)
   ) {
-
+    const key = keys[Math.floor(Math.random() * keys.length)]
+    dir = directions[key]
+    nextMovePos = position + dir.movement
   }
+  return { nextMovePos, direction: dir }
 }
