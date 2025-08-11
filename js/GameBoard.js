98c364f3721d45f20759777ce60a8c5676a2b71a
Nimira43 <n.rabone72@aol.com>
2025-08-11 18:29:16 +0100

    Gameboard class outline

---
+++ js/GameBoard.js
@@ -0,0 +1,16 @@
+class GameBoard {
+  constructor(DOMGrid) {
+
+  }
+
+  showGameStatus() {}
+  createLevel() {}
+  addObject() {}
+  removeObject() {}
+  objectExists() {}
+  rotateDiv() {}
+  moveCharacter() {}
+  createGameBoard() {}
+}
+
+export default GameBoard
