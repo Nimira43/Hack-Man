c3dc4217a0bc161e5b75cee08e88e537c3b80f66
Nimira43 <n.rabone72@aol.com>
2025-08-11 18:48:17 +0100

    Ghost constructor. Parameters for methods.

--- js/Ghost.js
+++ js/Ghost.js
@@ -3,13 +3,21 @@ import { randomMovement } from './ghostMovements'
 
 class Ghost {
   constructor(speed = 5, startPos, movement, name) {
-
+    this.name = name
+    this.movement = movement
+    this.startPos = startPos
+    this.pos = startPos
+    this.dir = directions.ArrowRight
+    this.speed = speed
+    this.timer = 0
+    this.isAlerted = false
+    this.rotation = false
   }
 
   shouldMove() {}
-  getNextMove() {}
+  getNextMove(objectExist) {}
   makeMove() {}
-  setNewPos() {}
+  setNewPos(nextMovePos, direction) {}
 }
 
 export default Ghost
