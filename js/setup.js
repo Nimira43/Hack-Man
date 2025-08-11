ae4741a1e14cb9fb426f6158173c5797cd47f438
Nimira43 <n.rabone72@aol.com>
2025-08-11 17:53:41 +0100

    gridSize, cellSize and directions variables

---
+++ js/setup.js
@@ -0,0 +1,25 @@
+export const gridSize = 20
+export const cellSize = 20
+
+export const directions = {
+  ArrowLeft: {
+    code: 37,
+    movement: -1,
+    rotation: 180
+  },
+  ArrowUp: {
+    code: 38,
+    movement: -gridSize,
+    rotation: 270
+  },
+  ArrowRight: {
+    code: 39,
+    movement: 1,
+    rotation: 0
+  },
+  ArrowDown: {
+    code: 40,
+    movement: gridSize,
+    rotation: 90
+  },
+}
