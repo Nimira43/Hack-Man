a700a2ec37f34a3741823b20d28365547c5a0f76
Nimira43 <n.rabone72@aol.com>
2025-08-11 17:47:24 +0100

    const variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Working')
+const gameGrid = document.querySelector('#game')
+const scoreTable = document.querySelector('#score')
+const startButton = document.querySelector('#start-btn')
+const powerPillTime = 10000
+const globalSpeed = 80
+
+// const gameBoard
+
