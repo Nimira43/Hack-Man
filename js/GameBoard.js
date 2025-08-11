71719db243b8f2952c8cd959207828d756344789
Nimira43 <n.rabone72@aol.com>
2025-08-11 18:41:51 +0100

    GameBoard constructor. Parameters for methods.

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -2,17 +2,19 @@ import { gridSize, cellSize, objectType, classList } from './setup'
 
 class GameBoard {
   constructor(DOMGrid) {
-
+    this.dotCount = 0
+    this.grid = []
+    this.DOMGrid = DOMGrid
   }
 
-  showGameStatus() {}
-  createLevel() {}
-  addObject() {}
-  removeObject() {}
-  objectExists() {}
-  rotateDiv() {}
-  moveCharacter() {}
-  createGameBoard() {}
+  showGameStatus(gameWin) {}
+  createLevel(level) {}
+  addObject(pos, classes) {}
+  removeObject(pos, classes) {}
+  objectExists(pos, object) {}
+  rotateDiv(pos, deg) {}
+  moveCharacter(character) {}
+  static createGameBoard(DOMGrid, level) {}
 }
 
 export default GameBoard
