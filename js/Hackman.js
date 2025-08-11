15f929a5569e42368679d2f6a729ec16ba0db5e6
Nimira43 <n.rabone72@aol.com>
2025-08-11 18:44:59 +0100

    Hackman constructor. Parameters for methods.

--- js/Hackman.js
+++ js/Hackman.js
@@ -2,14 +2,19 @@ import { objectType, directions } from './setup'
 
 class Hackman {
   constructor(speed, startPos) {
-
+    this.pos = startPos
+    this.speed = speed
+    this.dir = null
+    this.timer = 0
+    this.powerPill = false
+    this.rotation = true
   }
 
   shouldMove() {}
-  getNextMove() {}
+  getNextMove(objectExist) {}
   makeMove() {}
-  setNewPos() {}
-  handleKeyInput() {}
+  setNewPos(nextMovePos) {}
+  handleKeyInput(e, objectExist) {}
 }
 
 export default Hackman
